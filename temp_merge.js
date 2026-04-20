4d4a3b18811d4556a08919346e0943b631ea369b
xcsweb <15123953025@139.com>
2026-04-20 07:46:11 +0000

    chore: remove temp files

--- temp_merge.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-
-// We have the new logic from trae/solo-agent-SZ3q0O and the old logic from origin/main.
-// Since it's too complex to script, I'll just generate the entire file in python or node.
